f2c9656c67b0c379b6fed8351e1e0dc171fe762a
Longfei Shen <longfeishen@LongfeideAir.lan>
2018-02-10 21:39:34 +0800

    第一次提交

--- dog_vs_cat/problem_unittests.py
+++ dog_vs_cat/problem_unittests.py
@@ -9,18 +9,18 @@ def _print_success_message():
     print('Tests Passed')
 
 
-def test_folder_path(cifar10_dataset_folder_path):
-    assert cifar10_dataset_folder_path is not None,\
+def test_folder_path(dog_vs_cat_dataset_folder_path):
+    assert dog_vs_cat_dataset_folder_path is not None,\
         'Cifar-10 data folder not set.'
-    assert cifar10_dataset_folder_path[-1] != '/',\
+    assert dog_vs_cat_dataset_folder_path[-1] != '/',\
         'The "/" shouldn\'t be added to the end of the path.'
-    assert os.path.exists(cifar10_dataset_folder_path),\
+    assert os.path.exists(dog_vs_cat_dataset_folder_path),\
         'Path not found.'
-    assert os.path.isdir(cifar10_dataset_folder_path),\
-        '{} is not a folder.'.format(os.path.basename(cifar10_dataset_folder_path))
+    assert os.path.isdir(dog_vs_cat_dataset_folder_path),\
+        '{} is not a folder.'.format(os.path.basename(dog_vs_cat_dataset_folder_path))
 
-    train_files = [cifar10_dataset_folder_path + '/data_batch_' + str(batch_id) for batch_id in range(1, 6)]
-    other_files = [cifar10_dataset_folder_path + '/batches.meta', cifar10_dataset_folder_path + '/test_batch']
+    train_files = ['train']
+    other_files = []
     missing_files = [path for path in train_files + other_files if not os.path.exists(path)]
 
     assert not missing_files,\
